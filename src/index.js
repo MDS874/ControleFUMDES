50c3c51d3be4420d5930c3667dbf9a46ff2e67b9
MDS874 <80173005+MDS874@users.noreply.github.com>
2024-12-04 10:02:10 -0300

    Criacao Front

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { pdfjs } from "react-pdf";
+
+// Caminho correto para o worker com base na versão atual do pdfjs-dist
+pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf-workers/pdf.worker.min.js`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +15,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Medir performance da aplicação
 reportWebVitals();
